0dc2eebfab326e1c8ee3d8c546e64bfb110bb16e
Ashley Foster <ashley@ashleyfoster.me>
2018-01-17 14:52:40 -0500

    Add in design for the home page

--- lib/tutorlly_web/router.ex
+++ lib/tutorlly_web/router.ex
@@ -14,13 +14,8 @@ defmodule TutorllyWeb.Router do
   end
 
   scope "/", TutorllyWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", TutorllyWeb do
-  #   pipe_through :api
-  # end
 end
